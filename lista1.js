4dd5c28d1ced6d164c87963e5323c22fc928722e
nickdam <52571385+nickdam@users.noreply.github.com>
2020-09-28 14:06:52 -0300

    Add files via upload

---
+++ lista1.js
@@ -0,0 +1,38 @@
+function mudar(){
+var i;
+  for (i=1;i<11;i++){
+    alert ((document.getElementById("result").innerHTML = "mudou!") + " Número da vez: " + i);
+  }
+}
+function ligar1 (){
+  document.getElementById("luz1off"). src = 'lon.png';
+}
+function desligar1() {
+  document.getElementById("luz1off"). src = 'loff.png';
+}
+function ligar2 (){
+  document.getElementById("luz2off"). src = 'lon.png';
+}
+function desligar2() {
+  document.getElementById("luz2off"). src = 'loff.png';
+}function ligar3 (){
+  document.getElementById("luz3off"). src = 'lon.png';
+}
+function desligar3() {
+  document.getElementById("luz3off"). src = 'loff.png';
+}function ligar4 (){
+  document.getElementById("luz4off"). src = 'lon.png';
+}
+function desligar4() {
+  document.getElementById("luz4off"). src = 'loff.png';
+}function ligar5 (){
+  document.getElementById("luz5off"). src = 'lon.png';
+}
+function desligar5() {
+  document.getElementById("luz5off"). src = 'loff.png';
+}function ligar6 (){
+  document.getElementById("luz6off"). src = 'lon.png';
+}
+function desligar6() {
+  document.getElementById("luz6off"). src = 'loff.png';
+}
